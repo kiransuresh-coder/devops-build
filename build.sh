0621b098a0c4cc7ed5a791b8ab7e9e54bf7663f7
Kiran Kumar <109231951+kiransuresh-coder@users.noreply.github.com>
2026-01-10 12:50:52 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-docker build -t react-app:latest .
-echo "Docker image built successfully"
+ENV=$1
+IMAGE_NAME=kiransuresh12/react-app-$ENV
+
+echo "Building Docker image: $IMAGE_NAME"
+
+docker build -t $IMAGE_NAME:latest .
+
+echo "Pushing image to Docker Hub..."
+docker push $IMAGE_NAME:latest
