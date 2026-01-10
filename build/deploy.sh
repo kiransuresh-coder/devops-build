5b648aaedeaa754e54a5de21829abae5abeccd5d
Kiran Kumar <109231951+kiransuresh-coder@users.noreply.github.com>
2026-01-10 11:44:20 +0530

    Update deploy.sh

--- build/deploy.sh
+++ build/deploy.sh
@@ -4,21 +4,21 @@ ENV=$1
 echo "Starting deployment for environment: $ENV"
 
 # Stop & remove any old container (dev or prod)
-if docker ps -q -f name=my-react-dev; then
+if docker ps -q -f name=react-app-dev; then
   echo "Stopping my-react-dev..."
-  docker stop my-react-dev
-  docker rm my-react-dev
+  docker stop react-app-dev
+  docker rm react-app-dev
 fi
 
 if docker ps -q -f name=my-react-prod; then
-  echo "Stopping my-react-prod..."
-  docker stop my-react-prod
-  docker rm my-react-prod
+  echo "Stopping react-app-prod..."
+  docker stop react-app-prod
+  docker rm react-app-prod
 fi
 
 # Run new container with docker-compose
-echo "Launching my-react-$ENV..."
+echo "Launching react-app-$ENV..."
 ENVIRONMENT=$ENV docker-compose up -d --build
 
 # Verify container is running
-docker ps -f name=my-react-$ENV
+docker ps -f name=react-app-$ENV
