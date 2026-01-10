bea63a41e3fef387611e30aa5a58dd41dad6bb37
Kiran Kumar <109231951+kiransuresh-coder@users.noreply.github.com>
2026-01-10 11:40:34 +0530

    Update build.sh

--- build/build.sh
+++ build/build.sh
@@ -1,20 +1,3 @@
 #!/bin/bash
-
-ENV=$1
-
-if [ -z "$ENV" ]; then
-  echo "Usage: ./build.sh dev|prod"
-  exit 1
-fi
-
-IMAGE_NAME=kiransuresh12/react-app-$ENV
-
-echo "Building React app..."
-npm install
-npm run build
-
-echo "Building Docker image: $IMAGE_NAME"
-docker build -t $IMAGE_NAME:latest build/
-
-echo "Pushing image to Docker Hub..."
-docker push $IMAGE_NAME:latest
+docker build -t react-app:latest .
+echo "Docker image built successfully"
